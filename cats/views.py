4e631bb39329a6e63f377d565e60250d180bcae6
Vlad Vasiliev <vlad@example.com>
2024-10-09 13:53:28 +0300

    004

--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,5 @@
-from rest_framework import permissions, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.throttling import ScopedRateThrottle
 
@@ -12,8 +13,13 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,)
-    pagination_class = PageNumberPagination
-    # pagination_class = None
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    # pagination_class = PageNumberPagination
+    pagination_class = None
+    filterset_fields = ('color', 'birth_year',)
+    search_fields = ('$name', 'achievements__name',)
+    ordering_fields = ('name', 'birth_year',)
+    ordering = ('birth_year',)
     throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
     throttle_scope = 'low_request'
 
